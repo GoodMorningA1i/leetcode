5b6ea3dacd14588ce1c758984a7cd3dce811acc8
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-07-31 09:21:13 -0400

    Time: 20 ms (25.6%), Space: 22.6 MB (25.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,5 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # def isAnagram(word1, word2):
-        #     return word1.sort() == word2.sort()
-        
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:        
         result = defaultdict(list)
 
         for s in strs:
@@ -15,24 +12,6 @@ class Solution:
 
         return list(result.values())
 
-        # result = []
-
-        # sorted_strs = []
-        # for anagram in strs:
-        #     anagram.sort()
-        #     sorted_strs.append(anagram)
-
-        # while len(sorted_strs) > 0:
-        #     anagramList = []
-            
-
-        # return result
+       #Time Complexity: O(m*n)
+       #Space Complexity: O(m*n)
         
-    #Brute Force
-    #Time Complexity: m * nlogn, 
-    #n = length of each individual anagrma
-    #m is the length of strs
-    #Space Complexity: O()
-
-    #Optimal
-
