7b3de7fd4f958383a83d9310b9ca80bcb7b568f5
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-08-30 17:56:39 -0400

    Time: 7 ms (50.41%), Space: 24.2 MB (13.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,26 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        """
-        [1,2,3,4,1,2,3,1,2,1, 3], k = 2
-        [1,2] or [1,3] or doesn't matter
-
-        size, empty, how big
-        dichotomy, duplicates
-        boundary, ties
-        order, sorted
-        """
-
-        elementToOccurences = defaultdict(int)
+        numsToCount = defaultdict(int)
         for num in nums:
-            elementToOccurences[num] += 1
+            numsToCount[num] += 1
 
-        sortedItems = sorted(elementToOccurences.items(), key=lambda x: x[1], reverse=True)
+        countToNums = defaultdict(list)
+        freq = [[] for i in range(len(nums) + 1)]
+        for n, c in numsToCount.items():
+            freq[c].append(n)
+        
         result = []
         counter = 0
-        for item in sortedItems:
-            if counter == k:
-                break
-            result.append(item[0])
-            counter += 1
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                if counter == k:
+                    break
+                result.append(num)
+                counter += 1
 
         return result
