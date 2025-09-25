44741fa1b137cc3020c020bdfdcc299c9687f4f2
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-09-25 17:51:14 -0400

    Time: 0 ms (100%), Space: 20.6 MB (5.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -1,12 +1,26 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        for i in 0..<(nums.count) {
-            for j in (i + 1)..<(nums.count) {
-                if nums[i] + nums[j] == target {
-                    return [i,j]
-                }
+        """
+        {
+
+        }
+        """
+        var complements: [Int: Int] = [:]
+        for i in 0..<nums.count {
+            if complements[nums[i]] == nil {
+                complements[nums[i]] = i
             }
         }
-        return [] //never reach
+
+        for j in 0..<nums.count {
+            let complement = target - nums[j]
+            guard let i = complements[complement] else { 
+                continue
+            }
+            if i != j {
+                return [i, j]
+            }
+        }
+        return [] //will never reach
     }
 }
