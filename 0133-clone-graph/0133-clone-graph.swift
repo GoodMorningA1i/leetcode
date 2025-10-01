bd16fa5ce14dd727cf0d853acfd11727de1d4636
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-10-01 11:53:37 -0400

    Time: 7 ms (78.04%), Space: 18.2 MB (78.04%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var neighbors: [Node?]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.neighbors = []
+ *     }
+ * }
+ */
+
+class Solution {
+    func cloneGraph(_ node: Node?) -> Node? {
+        var oldToNew: [Node?: Node?] = [:]
+
+        func clone(_ node: Node?) -> Node? {
+            if let newNode = oldToNew[node] {
+                return newNode
+            }
+            guard let node = node else { return nil }
+            var copy = Node(node.val)
+            oldToNew[node] = copy
+            for neighbour in node.neighbors {
+                copy.neighbors.append(clone(neighbour))
+            }
+
+            return copy
+        }
+
+        return clone(node)
+    }
+}
