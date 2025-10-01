8237c6f4a98ce6f9f45560dce40a09b499edef2c
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-10-01 11:14:54 -0400

    Time: 324 ms (7.18%), Space: 25.4 MB (21.97%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,62 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
+        result = 0
         visit = set()
         rows = len(grid)
         columns = len(grid[0])
 
-        def bfs(r, c):
-            q = collections.deque()
-            q.append((r,c))
-            visit.add((r,c))
+        def bfs(row, column):
+            q = deque()
+            q.append((row, column))
+            visit.add((row, column))
+
             while q:
-                row, column = q.popleft()
-                directions = [[0,1], [0,-1], [-1,0], [1,0]]
+                r, c = q.popleft()
+                directions = [[1,0], [-1,0], [0,1], [0,-1]]
                 for dr, dc in directions:
-                    r,c = row + dr, column + dc
-                    if r in range(rows) and c in range(columns) and grid[r][c] == "1" and (r,c) not in visit:
-                        q.append((r,c))
-                        visit.add((r,c))
+                    updatedRow = r + dr
+                    updatedColumn = c + dc
+                    if updatedRow in range(rows) and updatedColumn in range(columns) and grid[updatedRow][updatedColumn] == "1" and (updatedRow, updatedColumn) not in visit:
+                        q.append((updatedRow, updatedColumn))
+                        visit.add((updatedRow, updatedColumn))
 
-        for r in range(0, rows):
-            for c in range(0, columns):
-                if grid[r][c] == "1" and (r,c) not in visit:
-                    bfs(r,c)
-                    islands += 1
+        for row in range(rows):
+            for column in range(columns):
+                if grid[row][column] == "1" and (row, column) not in visit:
+                    bfs(row, column)
+                    result += 1
 
-        return islands
-                
+        return result
 
-                
+
+"""
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+
+numIslands, grid be an array of an array of strings and returns an int
+
+
+[['1', '1', 1', '0', '0'],
+['1', '1', 0', '0', '0'],
+['1', '1', '1', '1', '0'],
+['0', '0', '0', '0', '1']]'
+
+output = 2
+
+BFS
+Time Complexity: O(m*n)
+Space Complexity: O(m*n)
+
+O(V+E)
+
+[0,0]: [[0,1], [1,0]]
+
+visited queue = what elements have you visited (set)
+to be visited = what elements do you still need to visit (queue)
+
+
+"""
