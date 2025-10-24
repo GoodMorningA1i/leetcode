1d2d4697e48bda55e47aaec6b8dd5344a395e7d7
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-10-24 17:38:03 -0400

    Time: 55 ms (5.11%), Space: 34.9 MB (17.3%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,24 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return False
+        numToOccurence = defaultdict(int)
+        for num in nums:
+            numToOccurence[num] += 1
 
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
+        for num in numToOccurence:
+            if numToOccurence[num] > 1:
                 return True
         
         return False
-
-        #Brute force
-        #Go through the entire array
-        #Create a dictionary with each of the elements
-        #If any of the dictionary values is greater than 1, than true
-        #Time Complexity: O(n + n) = O(2n) = O(n)
-        #Space Complexity: O(n)
-
-        #Optimize
-        #Binary sort
-        #Time Complexity: O(n) = O(nlogn + n)
-        #Space Complexity: O(1)
-
