1f67e27c0384c20c9f47dca9e8d59221c62a05c9
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-10-31 16:11:36 -0400

    Time: 0 ms (100%), Space: 18.8 MB (44.2%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,15 +3,16 @@ class Solution:
         index = -1
         l = 0
         r = len(nums) - 1
-
+        
         while l <= r:
             mid = (l + r) // 2
-            if nums[mid] < target:
-                l = mid + 1
-            elif nums[mid] > target:
+            if nums[mid] > target:
                 r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
             else:
                 index = mid
                 break
-        
+
         return index
+        
