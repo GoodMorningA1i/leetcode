9f291729fcc8674fd94ecd95e0c6b75cdb50f9ff
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-11-02 17:17:40 -0500

    Time: 0 ms (100%), Space: 17.9 MB (69.89%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,16 +1,29 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def recursiveSolve(curr, openBracketNum, closeBracketNum): 
-            if closeBracketNum == openBracketNum == n:
-                return [curr]
+        """
+        we can only add open or close when it less than or equal to n
+        if we have close < open, then we can add close
+        """
+        res = []
+        stack = []
+
+        def backtrack(openNumber, closeNumber):
+            #Valid IFF openNumber == closeNumber == n
+            if openNumber == closeNumber == n:
+                res.append("".join(stack))
+                return
             
-            openList, closeList = [], []
-            if openBracketNum < n:
-                openList = recursiveSolve(curr + '(', openBracketNum + 1, closeBracketNum)
-                
-            if closeBracketNum < openBracketNum:
-                closeList = recursiveSolve(curr + ')', openBracketNum, closeBracketNum + 1)
-                
-            return openList + closeList
-        
-        return recursiveSolve('', 0, 0)
+            #Only add open brackets if its under the limit
+            if openNumber < n:
+                stack.append('(')
+                backtrack(openNumber + 1, closeNumber)
+                stack.pop()
+
+            #Only add close brackets if its less than the # of open brackets
+            if closeNumber < openNumber:
+                stack.append(')')
+                backtrack(openNumber, closeNumber + 1)
+                stack.pop()
+
+        backtrack(0, 0) 
+        return res
