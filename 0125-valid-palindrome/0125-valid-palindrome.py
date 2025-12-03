39925e86b83acb80547d9128a8362c086942428f
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2025-12-03 08:53:45 -0500

    Time: 4 ms (90.83%), Space: 18.5 MB (34.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,52 +1,18 @@
-"""
-ata
-true
-
-atta
-true
-
-
-ate, eta
-true
-
-hello, bob
-false
-
-ate, eat
-false
-
-ate, ETA
-true
-
-ate, ETA_%#@
-true
-
-Test cases
-empty
-
-Start from the beginning
-Start from the end
-See if they make the same work
-O(2n) = O(n)
-O(n)
-
-pointer at start
-pointer at end
-see if they match
-"""
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alnum_s = ""
-        for char in s:
-            if char.isalnum():
-                alnum_s += char
+        lowerS = s.lower()
+        newSArr = []
+        for c in lowerS:
+            if c.isalnum():
+                newSArr.append(c)
+        convertedS = "".join(newSArr)
 
-        pointerA = 0
-        pointerB = len(alnum_s) - 1
-        while pointerA < pointerB:
-            if alnum_s[pointerA].lower() != alnum_s[pointerB].lower():
+        l = 0
+        r = len(convertedS) - 1
+        while l < r:
+            if convertedS[l] != convertedS[r]:
                 return False
-            pointerA += 1
-            pointerB -= 1
+            l += 1
+            r -= 1
+        
         return True
