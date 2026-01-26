6b9996e3f67e0ddadc3ff90b1fd141cd496a3f11
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2026-01-25 19:09:37 -0500

    Time: 152 ms (24.08%), Space: 69.2 MB (79.78%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,19 +1,16 @@
-from collections import defaultdict
-
 class TimeMap:
-
     def __init__(self):
-        self.map = defaultdict(list) #key: list of [value, timestamp]
-
-    def set(self, key: str, value: str, timestamp: int):
+        self.map = defaultdict(list) #list elements will be (value, timestamp)
+    
+    def set(self, key, value, timestamp):
         self.map[key].append((value, timestamp))
     
-    def get(self, key: str, timestamp: int) -> str:
-        res = ""
+    def get(self, key, timestamp):
         values = self.map[key]
 
         l = 0
         r = len(values) - 1
+        res = ""
 
         while l <= r:
             mid = (l + r) // 2
