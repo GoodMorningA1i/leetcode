0625dd682a9017ea5f55ac0f726ef92d2f8b4eda
Ali Syed <52847195+GoodMorningA1i@users.noreply.github.com>
2026-02-15 13:45:13 -0500

    Time: 0 ms (100%), Space: 19.2 MB (90.82%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func rob(_ nums: [Int]) -> Int {
+        var rob1 = 0
+        var rob2 = 0
+        for n in nums {
+            let temp = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = temp
+        }
+        return rob2
+    }
+}
